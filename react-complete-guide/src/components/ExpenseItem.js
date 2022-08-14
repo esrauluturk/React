c6c15ec603af1a0720e14085c08df6b0f2889826
Fatma Esra Ulutürk <feuuluturk@gmail.com>
2022-08-14 21:10:44 +0300

    JS kodları ile dinamik placeholder oluşturuldu -- toISOString() kullanıldı

--- react-complete-guide/src/components/ExpenseItem.js
+++ react-complete-guide/src/components/ExpenseItem.js
@@ -1,16 +1,19 @@
-import './ExpenseItem.css';
+import "./ExpenseItem.css";
 
-function ExpenseItem(){
-    return(
-        <div className='expense-item'>
-            <div>March 28th 2021</div>
-            <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
-                <div className='expense-item__price'>$294.67</div>
-            </div>
-        </div>
-        
-        
-    );
+function ExpenseItem() {
+  // js code
+  const expenseDate = new Date(2021, 2, 28);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmount = 294.67;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        {/* 1+1, Math.random() */}
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmount}</div>
+      </div>
+    </div>
+  );
 }
 export default ExpenseItem;
