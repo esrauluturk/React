da7f1b215b4dbdc60e0a6084237e516a073fd29e
Fatma Esra Ulutürk <feuuluturk@gmail.com>
2022-08-14 20:01:20 +0300

    Initial commit -- JS ve JSX yazım şekilleri karşılaştırıldı.

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  // JS de yazmak istersek :
+  const para = document.createElement('p');
+  para.textContent = 'This is also visible!';
+  document.getElementById('root').append(para);
+  
+  return (
+    <div>
+      <h2>Let's get started!</h2>
+      <p>This is also visible!</p>
+    
+    </div>
+  );
+}
+
+export default App;
